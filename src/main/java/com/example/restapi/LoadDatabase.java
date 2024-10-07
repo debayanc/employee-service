94572a3318ab112be3ed72bdc75e96828d876986
Debayan Chakrobortty <debayan.chakrobortty@gmail.com>
2024-10-07 16:26:24 +1100

    first commit

---
+++ src/main/java/com/example/restapi/LoadDatabase.java
@@ -0,0 +1,23 @@
+package com.example.restapi;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Configuration
+class LoadDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+        };
+    }
+}
